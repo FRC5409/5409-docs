cf04908ccbed9b24052176f18a997702c2065a8d
alexanderszura <1szuraale@hdsb.ca>
2025-04-17 19:35:34 -0400

    Init commit
    -Classic template

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,236 +1,151 @@
-const config = require('./contrib/config.js')
-const fs = require('fs')
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-const githubRepoName =
-  config.projectSlug === 'ecosystem' ? 'docs' : config.projectSlug
+import {themes as prismThemes} from 'prism-react-renderer';
 
-const baseUrl = config.baseUrl ? config.baseUrl : `/${config.projectSlug}/docs/`
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-let links = [
-  {
-    to: 'https://www.ory.sh/',
-    label: `Home`,
-    position: 'left'
-  }
-]
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
 
-if (config.openApiSpec) {
-  links = [
-    ...links,
-    {
-      to: baseUrl,
-      label: `Docs`,
-      position: 'left',
-      activeBaseRegex: 'docs/(?!http-api/).*'
-    },
-    {
-      to: baseUrl + 'http-api/',
-      label: `HTTP API`,
-      position: 'left'
-    }
-  ]
-}
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
 
-links = [
-  ...links,
-  {
-    href: `https://github.com/ory/${githubRepoName}/discussions`,
-    label: 'Discussions',
-    position: 'right'
-  },
-  {
-    href: 'https://www.ory.sh/chat',
-    label: 'Slack',
-    position: 'right'
-  },
-  {
-    href: `https://github.com/ory/${githubRepoName}`,
-    label: 'GitHub',
-    position: 'right'
-  }
-]
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
-const customCss = [require.resolve('./contrib/theme.css')]
-
-if (fs.existsSync('./src/css/theme.css')) {
-  customCss.push(require.resolve('./src/css/theme.css'))
-}
-
-const githubPrismTheme = require('prism-react-renderer/themes/github')
-
-const prismThemeLight = {
-  ...githubPrismTheme,
-  styles: [
-    ...githubPrismTheme.styles,
-    {
-      languages: ['keto-relation-tuples'],
-      types: ['namespace'],
-      style: {
-        color: '#666'
-      }
-    },
-    {
-      languages: ['keto-relation-tuples'],
-      types: ['object'],
-      style: {
-        color: '#939'
-      }
-    },
-    {
-      languages: ['keto-relation-tuples'],
-      types: ['relation'],
-      style: {
-        color: '#e80'
-      }
-    },
-    {
-      languages: ['keto-relation-tuples'],
-      types: ['delimiter'],
-      style: {
-        color: '#555'
-      }
-    },
-    {
-      languages: ['keto-relation-tuples'],
-      types: ['comment'],
-      style: {
-        color: '#999'
-      }
-    },
-    {
-      languages: ['keto-relation-tuples'],
-      types: ['subject'],
-      style: {
-        color: '#903'
-      }
-    }
-  ]
-}
-
-module.exports = {
-  title: config.projectName,
-  tagline: config.projectTagLine,
-  url: `https://www.ory.sh/`,
-  baseUrl,
-  favicon: 'img/favico.png',
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  organizationName: 'ory', // Usually your GitHub org/user name.
-  projectName: config.projectSlug, // Usually your repo name.
-  themeConfig: {
-    prism: {
-      theme: prismThemeLight,
-      darkTheme: require('prism-react-renderer/themes/dracula'),
-      additionalLanguages: ['pug', 'shell-session']
-    },
-    announcementBar: {
-      id: 'supportus',
-      content:
-        config.projectSlug === 'docs'
-          ? `Sign up for <a href="${config.newsletter}">important security announcements</a> and if you like the ${config.projectName} give us some ⭐️ on <a target="_blank" rel="noopener noreferrer" href="https://github.com/ory">GitHub</a>!`
-          : `Sign up for <a href="${config.newsletter}">important security announcements</a> and if you like ${config.projectName} give it a ⭐️ on <a target="_blank" rel="noopener noreferrer" href="https://github.com/ory/${githubRepoName}">GitHub</a>!`
-    },
-    algolia: {
-      apiKey: '8463c6ece843b377565726bb4ed325b0',
-      indexName: 'ory',
-      contextualSearch: true,
-      searchParameters: {
-        facetFilters: [[`tags:${config.projectSlug}`, `tags:docs`]]
-      }
-    },
-    navbar: {
-      hideOnScroll: false,
-      logo: {
-        alt: config.projectName,
-        src: `img/logo-${config.projectSlug}.svg`,
-        srcDark: `img/logo-${config.projectSlug}.svg`,
-        href:
-          config.projectSlug === 'docs'
-            ? `https://www.ory.sh`
-            : `https://www.ory.sh/${config.projectSlug}`
-      },
-      items: [
-        ...links,
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
-          dropdownActiveClassDisabled: true,
-          dropdownItemsAfter: [
-            {
-              to: '/versions',
-              label: 'All versions'
-            }
-          ]
-        }
-      ]
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} ORY GmbH`,
-      links: [
-        {
-          title: 'Company',
-          items: [
-            {
-              label: 'Imprint',
-              href: 'https://www.ory.sh/imprint'
-            },
-            {
-              label: 'Privacy',
-              href: 'https://www.ory.sh/privacy'
-            },
-            {
-              label: 'Terms',
-              href: 'https://www.ory.sh/tos'
-            }
-          ]
-        }
-      ]
-    }
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
-  plugins: [
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        path:
-          config.projectSlug === 'docusaurus-template'
-            ? 'contrib/docs'
-            : 'docs',
-        sidebarPath: require.resolve('./contrib/sidebar.js'),
-        editUrl: ({ docPath }) =>
-          `https://github.com/ory/${githubRepoName}/edit/master/docs/docs/${docPath}`,
-        routeBasePath: '/',
-        showLastUpdateAuthor: true,
-        showLastUpdateTime: true,
-        disableVersioning: false
-      }
-    ],
-    '@docusaurus/plugin-content-pages',
-    require.resolve('./src/plugins/docusaurus-plugin-matamo'),
-    '@docusaurus/plugin-sitemap'
-  ],
-  themes: [
-    [
-      '@docusaurus/theme-classic',
-      {
-        customCss
-      }
-    ],
-    '@docusaurus/theme-search-algolia'
-  ],
+
   presets: [
     [
-      'redocusaurus',
-      {
-        specs: config.openApiSpec
-          ? [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
               {
-                routePath: '/http-api/',
-                specUrl: config.openApiSpec
-              }
-            ]
-          : []
-      }
-    ]
-  ]
-}
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
